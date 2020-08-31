779284482423e7ef2f6d9f523fe39372f72f93eb
Coder Srinivas <srinivas.babu364@gmail.com>
2020-08-31 14:44:45 +0530

    Setup Code for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,21 @@
+console.log("Client Side Javascript");
 
-console.log("Client Side Javascript")
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const forecast = document.getElementById("forecast");
 
+button.onclick = (event) => {
+  event.preventDefault();
+  forecast.innerHTML = "Loading...";
+  const address = input.value;
 
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-const forecast = document.getElementById('forecast')
+  const url = "/weather?address=" + address;
 
-button.onclick = (event)=> {
-
-    event.preventDefault()
-    const address = input.value;
-
-    const url = 'http://localhost:3000/weather?address=' + address
-
-    fetch(url).then((response) => {
+  fetch(url).then((response) => {
     response.json().then((data) => {
-
-        if(data.error){
-            forecast.innerHTML = data.error
-        }
-        else
-        forecast.innerHTML = data.forecast
-    })
-})
-}
-
+      if (data.error) {
+        forecast.innerHTML = data.error;
+      } else forecast.innerHTML = data.forecast;
+    });
+  });
+};
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
 
 const app = express();
+const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 const partialDirectoryPath = path.join(__dirname, "../templates/partials");
@@ -70,6 +71,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Listenning on port 3000");
+app.listen(port, () => {
+  console.log("Listenning on port " + port);
 });
